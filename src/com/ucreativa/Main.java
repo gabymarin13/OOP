4c451a0d0d0b7cc6e14f3d77ed3304b10f791c6c
gabymarin13 <ana.marin.chaves@ucreativa.com>
2021-05-31 20:50:52 -0600

    Creación de Familiar y Amigo que heredan de Persona

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -1,22 +1,30 @@
 package com.ucreativa;
 
-import com.ucreativa.familia.*;
+//import com.ucreativa.familia.*;
+import com.ucreativa.vacunacion.*;
+
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Gabriela yo = new Gabriela("Estoy heredando de mi madre");
+        Persona yo = new Persona("Gabriela Marín","115200123", 28, true);
+        Persona mama = new Persona("Violeta Chaves","125232326", 63, true);
+        Amigo amigoTrabajo = new Amigo("Esteban Quirós", "56988888", 25, false, "Amigo", "Esteban.fb25");
+        Familiar novio = new Familiar("Jose Araya", "114588966", 33, false, "Pareja");
 
-        //Violeta madre = new Violeta ("Hola mi chiquita");
+        List<Persona> persona = new ArrayList<>();
+        persona.add(yo);
+        persona.add(mama);
+        persona.add(amigoTrabajo);
+        persona.add(novio);
 
-        //madre.setAge();
+        List<BitacoraVacuna> bitacora = new ArrayList<>();
+        bitacora.add(new BitacoraVacuna(yo, "Pfizer", new Date()));
+        bitacora.add(new BitacoraVacuna(mama, "Pfizer", new Date()));
+        bitacora.add(new BitacoraVacuna(amigoTrabajo, "AstraZeneca", new Date()));
+        bitacora.add(new BitacoraVacuna(novio, "J&J", new Date()));
 
-        //int age = madre.getAge();
-
-        //System.out.println("Acabo de cumplir " + age);
-        //System.out.println("Y mi hobby favorito es: " + madre.getHobby());
-
-	    System.out.println(yo);
     }
 }
